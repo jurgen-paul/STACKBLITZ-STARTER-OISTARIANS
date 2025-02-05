38cd40f5e6ca272aa823fc279794ad3b3d8b0a29
Divyanshu Verma <73750407+divyanshuverma@users.noreply.github.com>
2025-02-05 14:40:13 +0530

    Added .env

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const config = {
   apiKey: process.env.API_KEY,
   serverSecret: process.env.SERVER_SECRET,
   isKalvian: process.env.IS_KALVIAN === 'true',
+  databaseURL: process.env.DATABASE_URL,
 };
 module.exports = config;
 console.log(config);
